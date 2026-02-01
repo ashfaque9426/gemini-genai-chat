bc3a60ba747c942bd0ee3f771f79b984f1c6fe78
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2026-02-02 02:52:56 +0600

    /api/get-conversation slightly modified to remove document ObjectId which is not required for LLM input.

--- app/api/get-conversation/route.ts
+++ app/api/get-conversation/route.ts
@@ -21,7 +21,7 @@ export async function GET(req: NextRequest) {
 
         const convID = new Types.ObjectId(convId);
 
-        const result = await Message.find({ conversationId: convID, uid: decoded.uid }).lean();
+        const result = await Message.find({ conversationId: convID, uid: decoded.uid }).select("role content -_id").lean();
 
         if (!result.length) {
             throw new Error("Unable to find messages. Please try again later");
